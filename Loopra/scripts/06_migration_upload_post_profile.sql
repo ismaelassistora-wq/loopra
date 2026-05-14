0319539395f7002a79cbb1f9949e977d192cd835
ismaelassistora-wq <ismaelassistora@gmail.com>
2026-05-14 11:25:14 +0200

    Delete Loopra directory

--- Loopra/scripts/06_migration_upload_post_profile.sql
+++
@@ -1,8 +0,0 @@
--- 06_migration_upload_post_profile.sql
-
--- Añadir la columna a la tabla clients para guardar el perfil de Upload-Post
-ALTER TABLE public.clients
-ADD COLUMN upload_post_profile TEXT;
-
--- Añadir comentario descriptivo
-COMMENT ON COLUMN public.clients.upload_post_profile IS 'Nombre del perfil creado en app.upload-post.com para publicar en redes sociales';
